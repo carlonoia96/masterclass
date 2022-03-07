ecb0dbb22f955675ed4efe3e240a4f79c4c1b8c5
carlnoia96 <carlokreosoft@gmail.com>
2022-03-07 18:36:48 +0100

    start home

---
+++ masterclass/src/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react';
+import './Home.module.css';
+
+interface HomeProps {
+    setCurrentRoute: (menu: string) => void;
+}
+
+class Home extends React.Component<HomeProps> {
+    render() {
+        return (<>
+
+        </>);
+    }
+}
+
+export default Home;
